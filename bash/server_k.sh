630406b21d764e164f68b0851b3d0bb2147558e6
Ansh09-chawla <ansh.jhande@gmail.com>
2023-10-08 12:15:47 -0600

    uploading banking system project

---
+++ bash/server_k.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar "/home/ubuntu/559/server_k.jar" -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1046
---
+++ bash/server_read.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar "/home/ubuntu/559/server_read.jar" -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=1047
